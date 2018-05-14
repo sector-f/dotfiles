c1276c6c1db4e9d40ea3912f895a92ce9ecf5427
Adam <sector-f@users.noreply.github.com>
2018-05-14 19:37:01 -0400

    Update bspwm configuration + directory

--- bspwm/.config/bspwm/panel/logout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#ToDo: Make dzen popup that asks if you want to shutdown/logoff/cancel
-
-echo "Hopefully this is a title"
-echo "Hopefully this isn't"
-echo "Hopefully this isn't either"
